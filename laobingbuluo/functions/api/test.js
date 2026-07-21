e5bced82d9f0bf75cc9b94afb4d9dd9655910927
oldmancanfly <1070344671@qq.com>
2026-07-21 08:08:47 +0800

    Delete laobingbuluo directory

--- laobingbuluo/functions/api/test.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ status: 'ok', message: '函数运行正常' }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
